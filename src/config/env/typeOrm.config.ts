3267b48c7a4d5a0fcb06318f0cbf42e3e5dc69f2
Lucas Rodrigues de Oliveira <lucasroliveira98@gmail.com>
2022-07-26 18:37:01 -0300

    fix: resolved docker problem with postgres

--- src/config/env/typeOrm.config.ts
+++ src/config/env/typeOrm.config.ts
@@ -10,12 +10,10 @@ export const typeOrmLoadEnv = (): { database: TypeOrmConfig } => {
   const {
     DATABASE_HOST: host,
     DATABASE_PORT: port,
-    DATABASE_USERNAME: username,
+    DATABASE_USER: username,
     DATABASE_PASSWORD: password,
     DATABASE_NAME: database,
   } = process.env;
-
-  console.log(process.env);
 
   return {
     database: {
